8852fe28dac520c89d31d828a544f08dd03c242b
GabrielEdJansen <93282987+GabrielEdJansen@users.noreply.github.com>
2021-11-17 21:48:20 -0300

    Update bpm.js

--- bpm.js
+++ bpm.js
@@ -92,33 +92,82 @@ function _init(data, info) {
             const Proj = map.get("Proj");
             const ConFin = map.get("ConFin");
             const CenCus = map.get("CenCus");
-           // const VlrTot = map.get("VlrTot");
             const DatPag = map.get("DatPag");
             const NumNf = map.get("NumNf");
             const DatPre = map.get("DatPre");
             const ObsPag = map.get("ObsPag");
+
             const DescParUm = map.get("DescParUm");
             const VlrParUm = map.get("VlrParUm");
             const DatParUm = map.get("DatParUm");
             const CodBolUm = map.get("CodBolUm");
+
             const DescParDois = map.get("DescParDois");
             const VlrParDois = map.get("VlrParDois");
             const DatParDois = map.get("DatParDois");
             const CodBolDois = map.get("CodBolDois");
+
             const DescParTres = map.get("DescParTres");
             const VlrParTres = map.get("VlrParTres");
             const DatParTres = map.get("DatParTres");
             const CodBolTres = map.get("CodBolTres");
+
+            const DescParQuatro = map.get("DescParQuatro");
+            const VlrParQuatro = map.get("VlrParQuatro");
+            const DatParQuatro = map.get("DatParQuatro");
+            const CodBolQuatro = map.get("CodBolQuatro");
+
+            const DescParCinco = map.get("DescParCinco");
+            const VlrParCinco = map.get("VlrParCinco");
+            const DatParCinco = map.get("DatParCinco");
+            const CodBolCinco = map.get("CodBolCinco");
+
+            const DescParSeis = map.get("DescParSeis");
+            const VlrParSeis = map.get("VlrParSeis");
+            const DatParSeis = map.get("DatParSeis");
+            const CodBolSeis = map.get("CodBolSeis");
+
+            const DescParSete = map.get("DescParSete");
+            const VlrParSete = map.get("VlrParSete");
+            const DatParSete = map.get("DatParSete");
+            const CodBolSete = map.get("CodBolSete");
+
+            const DescParOito = map.get("DescParOito");
+            const VlrParOito = map.get("VlrParOito");
+            const DatParOito = map.get("DatParOito");
+            const CodBolOito = map.get("CodBolOito");
+
+            const DescParNove = map.get("DescParNove");
+            const VlrParNove = map.get("VlrParNove");
+            const DatParNove = map.get("DatParNove");
+            const CodBolNove = map.get("CodBolNove");
+
+            const DescParDez = map.get("DescParDez");
+            const VlrParDez = map.get("VlrParDez");
+            const DatParDez = map.get("DatParDez");
+            const CodBolDez = map.get("CodBolDez");
+
+            const DescParOnze = map.get("DescParOnze");
+            const VlrParOnze = map.get("VlrParOnze");
+            const DatParOnze = map.get("DatParOnze");
+            const CodBolOnze = map.get("CodBolOnze");
+
+            const DescParDoze = map.get("DescParDoze");
+            const VlrParDoze = map.get("VlrParDoze");
+            const DatParDoze = map.get("DatParDoze");
+            const CodBolDoze = map.get("CodBolDoze");
+
             const TotalPagarPar = map.get("TotalPagarPar");
             const ForPagBanco = map.get("ForPagBanco");
             const ForPagAgencia = map.get("ForPagAgencia");
             const ForPagConta = map.get("ForPagConta");
-            const ForPagCodBarBoleto = map.get("ForPagCodBarBoleto");
             const ForPagChavePix = map.get("ForPagChavePix");
-           /* const NomSuper = map.get("NomSuper");
+
+            const NomSuper = map.get("NomSuper");
             const SupAproJusti = map.get("SupAproJusti");
             const NomDir = map.get("NomDir");
-            const DirAproJusti = map.get("DirAproJusti");*/
+            const DirAproJusti = map.get("DirAproJusti");
+
 
 
             document.getElementById("selectTipFor").setAttribute("value", selectTipFor);
@@ -128,33 +177,81 @@ function _init(data, info) {
             document.getElementById("Proj").setAttribute("value", Proj);
             document.getElementById("ConFin").setAttribute("value", ConFin);
             document.getElementById("CenCus").setAttribute("value", CenCus);
-           // document.getElementById("VlrTot").setAttribute("value", VlrTot);
             document.getElementById("DatPag").setAttribute("value", DatPag);
             document.getElementById("NumNf").setAttribute("value", NumNf);
             document.getElementById("DatPre").setAttribute("value", DatPre);
             document.getElementById("ObsPag").setAttribute("value", ObsPag);
+
             document.getElementById("DescParUm").setAttribute("value", DescParUm);
             document.getElementById("VlrParUm").setAttribute("value", VlrParUm);
             document.getElementById("DatParUm").setAttribute("value", DatParUm);
             document.getElementById("CodBolUm").setAttribute("value", CodBolUm);
+
             document.getElementById("DescParDois").setAttribute("value", DescParDois);
             document.getElementById("VlrParDois").setAttribute("value", VlrParDois);
             document.getElementById("DatParDois").setAttribute("value", DatParDois);
             document.getElementById("CodBolDois").setAttribute("value", CodBolDois);
+
             document.getElementById("DescParTres").setAttribute("value", DescParTres);
             document.getElementById("VlrParTres").setAttribute("value", VlrParTres);
             document.getElementById("DatParTres").setAttribute("value", DatParTres);
             document.getElementById("CodBolTres").setAttribute("value", CodBolTres);
+
+            document.getElementById("DescParQuatro").setAttribute("value", DescParQuatro);
+            document.getElementById("VlrParQuatro").setAttribute("value", VlrParQuatro);
+            document.getElementById("DatParQuatro").setAttribute("value", DatParQuatro);
+            document.getElementById("CodBolQuatro").setAttribute("value", CodBolQuatro);
+
+            document.getElementById("DescParCinco").setAttribute("value", DescParCinco);
+            document.getElementById("VlrParCinco").setAttribute("value", VlrParCinco);
+            document.getElementById("DatParCinco").setAttribute("value", DatParCinco);
+            document.getElementById("CodBolCinco").setAttribute("value", CodBolCinco);
+
+            document.getElementById("DescParSeis").setAttribute("value", DescParSeis);
+            document.getElementById("VlrParSeis").setAttribute("value", VlrParSeis);
+            document.getElementById("DatParSeis").setAttribute("value", DatParSeis);
+            document.getElementById("CodBolSeis").setAttribute("value", CodBolSeis);
+
+            document.getElementById("DescParSete").setAttribute("value", DescParSete);
+            document.getElementById("VlrParSete").setAttribute("value", VlrParSete);
+            document.getElementById("DatParSete").setAttribute("value", DatParSete);
+            document.getElementById("CodBolSete").setAttribute("value", CodBolSete);
+
+            document.getElementById("DescParOito").setAttribute("value", DescParOito);
+            document.getElementById("VlrParOito").setAttribute("value", VlrParOito);
+            document.getElementById("DatParOito").setAttribute("value", DatParOito);
+            document.getElementById("CodBolOito").setAttribute("value", CodBolOito);
+
+            document.getElementById("DescParNove").setAttribute("value", DescParNove);
+            document.getElementById("VlrParNove").setAttribute("value", VlrParNove);
+            document.getElementById("DatParNove").setAttribute("value", DatParNove);
+            document.getElementById("CodBolNove").setAttribute("value", CodBolNove);
+
+            document.getElementById("DescParDez").setAttribute("value", DescParDez);
+            document.getElementById("VlrParDez").setAttribute("value", VlrParDez);
+            document.getElementById("DatParDez").setAttribute("value", DatParDez);
+            document.getElementById("CodBolDez").setAttribute("value", CodBolDez);
+
+            document.getElementById("DescParOnze").setAttribute("value", DescParOnze);
+            document.getElementById("VlrParOnze").setAttribute("value", VlrParOnze);
+            document.getElementById("DatParOnze").setAttribute("value", DatParOnze);
+            document.getElementById("CodBolOnze").setAttribute("value", CodBolOnze);
+
+            document.getElementById("DescParDoze").setAttribute("value", DescParDoze);
+            document.getElementById("VlrParDoze").setAttribute("value", VlrParDoze);
+            document.getElementById("DatParDoze").setAttribute("value", DatParDoze);
+            document.getElementById("CodBolDoze").setAttribute("value", CodBolDoze);
+
             document.getElementById("TotalPagarPar").setAttribute("value", TotalPagarPar);
             document.getElementById("ForPagBanco").setAttribute("value", ForPagBanco);
             document.getElementById("ForPagAgencia").setAttribute("value", ForPagAgencia);
             document.getElementById("ForPagConta").setAttribute("value", ForPagConta);
-            document.getElementById("ForPagCodBarBoleto").setAttribute("value", ForPagCodBarBoleto);
             document.getElementById("ForPagChavePix").setAttribute("value", ForPagChavePix);
-          /*  document.getElementById("NomSuper").setAttribute("value", NomSuper);
+
+            document.getElementById("NomSuper").setAttribute("value", NomSuper);
             document.getElementById("SupAproJusti").setAttribute("value", SupAproJusti);
             document.getElementById("NomDir").setAttribute("value", NomDir);
-            document.getElementById("DirAproJusti").setAttribute("value", DirAproJusti);*/
+            document.getElementById("DirAproJusti").setAttribute("value", DirAproJusti);
         }
     });
 }
@@ -177,33 +274,81 @@ function _saveData(data, info) {
     newData.Proj = document.getElementById("Proj").value;
     newData.ConFin = document.getElementById("ConFin").value;
     newData.CenCus = document.getElementById("CenCus").value;
-   // newData.VlrTot = document.getElementById("VlrTot").value;
     newData.DatPag = document.getElementById("DatPag").value;
     newData.NumNf = document.getElementById("NumNf").value;
     newData.DatPre = document.getElementById("DatPre").value;
     newData.ObsPag = document.getElementById("ObsPag").value;
+
     newData.DescParUm = document.getElementById("DescParUm").value;
     newData.VlrParUm = document.getElementById("VlrParUm").value;
     newData.DatParUm = document.getElementById("DatParUm").value;
     newData.CodBolUm = document.getElementById("CodBolUm").value;
+
     newData.DescParDois = document.getElementById("DescParDois").value;
     newData.VlrParDois = document.getElementById("VlrParDois").value;
     newData.DatParDois = document.getElementById("DatParDois").value;
     newData.CodBolDois = document.getElementById("CodBolDois").value;
+
     newData.DescParTres = document.getElementById("DescParTres").value;
     newData.VlrParTres = document.getElementById("VlrParTres").value;
     newData.DatParTres = document.getElementById("DatParTres").value;
     newData.CodBolTres = document.getElementById("CodBolTres").value;
+
+    newData.DescParQuatro = document.getElementById("DescParQuatro").value;
+    newData.VlrParQuatro = document.getElementById("VlrParQuatro").value;
+    newData.DatParQuatro = document.getElementById("DatParQuatro").value;
+    newData.CodBolQuatro = document.getElementById("CodBolQuatro").value;
+
+    newData.DescParCinco = document.getElementById("DescParCinco").value;
+    newData.VlrParCinco = document.getElementById("VlrParCinco").value;
+    newData.DatParCinco = document.getElementById("DatParCinco").value;
+    newData.CodBolCinco = document.getElementById("CodBolCinco").value;
+
+    newData.DescParSeis = document.getElementById("DescParSeis").value;
+    newData.VlrParSeis = document.getElementById("VlrParSeis").value;
+    newData.DatParSeis = document.getElementById("DatParSeis").value;
+    newData.CodBolSeis = document.getElementById("CodBolSeis").value;
+
+    newData.DescParSete = document.getElementById("DescParSete").value;
+    newData.VlrParSete = document.getElementById("VlrParSete").value;
+    newData.DatParSete = document.getElementById("DatParSete").value;
+    newData.CodBolSete = document.getElementById("CodBolSete").value;
+
+    newData.DescParOito = document.getElementById("DescParOito").value;
+    newData.VlrParOito = document.getElementById("VlrParOito").value;
+    newData.DatParOito = document.getElementById("DatParOito").value;
+    newData.CodBolOito = document.getElementById("CodBolOito").value;
+
+    newData.DescParNove = document.getElementById("DescParNove").value;
+    newData.VlrParNove = document.getElementById("VlrParNove").value;
+    newData.DatParNove = document.getElementById("DatParNove").value;
+    newData.CodBolNove = document.getElementById("CodBolNove").value;
+
+    newData.DescParDez = document.getElementById("DescParDez").value;
+    newData.VlrParDez = document.getElementById("VlrParDez").value;
+    newData.DatParDez = document.getElementById("DatParDez").value;
+    newData.CodBolDez = document.getElementById("CodBolDez").value;
+
+    newData.DescParOnze = document.getElementById("DescParOnze").value;
+    newData.VlrParOnze = document.getElementById("VlrParOnze").value;
+    newData.DatParOnze = document.getElementById("DatParOnze").value;
+    newData.CodBolOnze = document.getElementById("CodBolOnze").value;
+
+    newData.DescParDoze = document.getElementById("DescParDoze").value;
+    newData.VlrParDoze = document.getElementById("VlrParDoze").value;
+    newData.DatParDoze = document.getElementById("DatParDoze").value;
+    newData.CodBolDoze = document.getElementById("CodBolDoze").value;
+
     newData.TotalPagarPar = document.getElementById("TotalPagarPar").value;
     newData.ForPagBanco = document.getElementById("ForPagBanco").value;
     newData.ForPagAgencia = document.getElementById("ForPagAgencia").value;
     newData.ForPagConta = document.getElementById("ForPagConta").value;
-    newData.ForPagCodBarBoleto = document.getElementById("ForPagCodBarBoleto").value;
     newData.ForPagChavePix = document.getElementById("ForPagChavePix").value;
-  /*  newData.NomSuper = document.getElementById("NomSuper").value;
+
+    newData.NomSuper = document.getElementById("NomSuper").value;
     newData.SupAproJusti = document.getElementById("SupAproJusti").value;
     newData.NomDir = document.getElementById("NomDir").value;
-    newData.DirAproJusti = document.getElementById("DirAproJusti").value;*/
+    newData.DirAproJusti = document.getElementById("DirAproJusti").value;
 
     console.log(newData);
     return {
